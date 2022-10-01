ab58bff4bf13e97dd9436e7ed423d5f229691ad3
ydaikai <yd0202@outlook.com>
2022-10-02 04:13:14 +0900

    Updated

--- my-wave-portal/scripts/run.js
+++ my-wave-portal/scripts/run.js
@@ -1,29 +1,35 @@
 const main = async () => {
-	const [owner, randomPerson] = await hre.ethers.getSigners();
-	const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-	const waveContract = await waveContractFactory.deploy();
-	await waveContract.deployed();
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+  console.log("Contract addy:", waveContract.address);
 
-	console.log("Contract deployed to:", waveContract.address);
-	console.log("Contract deployed by:", owner.address);
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+  console.log(waveCount.toNumber());
 
-	let waveCount;
-	waveCount = await waveContract.getTotalWaves();
+  /**
+   * Let's send a few waves!
+   */
+  let waveTxn = await waveContract.wave("A message!");
+  await waveTxn.wait(); // Wait for the transaction to be mined
 
-	let waveTxn = await waveContract.wave();
+  const [_, randomPerson] = await hre.ethers.getSigners();
+  waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
+  await waveTxn.wait(); // Wait for the transaction to be mined
 
-	waveCount = await waveContract.getTotalWaves();
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
 };
 
 const runMain = async () => {
   try {
     await main();
-    process.exit(0); // exit Node process without error
+    process.exit(0);
   } catch (error) {
     console.log(error);
-    process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
+    process.exit(1);
   }
-  // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
 };
 
 runMain();
